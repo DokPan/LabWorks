af69ee91420e866c045ecfbe01aa079154e9d0b1
Alina <alinaroshchina5@gmail.com>
2025-02-22 12:52:41 +0300

    Create LabWork4.1.cpp

---
+++ LabWork4/LabWork4.1.cpp
@@ -0,0 +1,35 @@
+int[] coins = { 10, 5, 2, 1 };
+Console.Write("Введите сумму для сдачи: ");
+int amount =30;
+
+if (amount < 0)
+{
+    Console.Write("Некорректная сумма");
+    return;
+}
+List<int> result = Change(coins, amount);
+
+if (result.Count > 0)
+{
+    Console.WriteLine("Монеты для сдачи: ");
+    foreach (int coin in result)
+        Console.WriteLine(coin);
+}
+else
+    Console.WriteLine("Сдача невозможна");
+
+static List<int> Change(int[] coins, int amount)
+{
+    List<int> change = new List<int>();
+    foreach (int coin in coins)
+    {
+        while (amount >= coin)
+        {
+            amount -= coin;
+            change.Add(coin);
+        }
+    }
+    if (amount != 0)
+        return new List<int>();
+    return change;
+}
