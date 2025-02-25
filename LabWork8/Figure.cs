e87c93a37fd598c0c79eca012701b452f37b7e7e
Alina <alinaroshchina5@gmail.com>
2025-02-25 12:00:20 +0300

    Abstract Class

---
+++ LabWork8/Figure.cs
@@ -0,0 +1,10 @@
+abstract class Figure
+{
+    public abstract int GetSquare();
+
+    public abstract int GetPerimetr();
+
+    public abstract void Print();
+
+    public abstract string Name { get; }
+}
