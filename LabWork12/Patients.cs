2c6085bafb4e42e26a4cbdd5fc6f99edeaf1eb0e
Alina <alinaroshchina5@gmail.com>
2025-02-28 14:17:02 +0300

    Record

---
+++ LabWork12/Patients.cs
@@ -0,0 +1,13 @@
+record Patients
+{
+    public string name;
+    public int id;
+    public int birthYear;
+
+    public Patients(string name, int id, int birthYear)
+    {
+        this.name = name;
+        this.id = id;
+        this.birthYear = birthYear;
+    }
+}
