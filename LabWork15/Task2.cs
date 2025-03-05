95000ee7660602e26790c75fd4f9f145a996307a
Alina <alinaroshchina5@gmail.com>
2025-03-05 10:40:14 +0300

    Create Task2.cs

---
+++ LabWork15/Task2.cs
@@ -0,0 +1,16 @@
+MathOperation mathOperations = (a, b) => Console.WriteLine($"Сумма: {a + b}");
+mathOperations += (a, b) => Console.WriteLine($"Разность: {a - b}");
+mathOperations += (a, b) => Console.WriteLine($"Произведение: {a * b}");
+mathOperations += (a, b) =>
+{
+    if (b != 0)
+        Console.WriteLine($"Частное: {a / (double)b}");
+    else
+        Console.WriteLine("Деление на 0 невозможно");
+};
+
+Console.WriteLine("Введите два числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+mathOperations(a, b);
