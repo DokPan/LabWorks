1ed62ab86bfa0936c260f525914289e73b8f810f
Alina <alinaroshchina5@gmail.com>
2025-03-29 10:51:56 +0300

    Add files via upload

---
+++ LabWork23/LabWorkLibrary/Maths.cs
@@ -0,0 +1,49 @@
+﻿namespace LabWorkLibrary
+{
+    /// <summary>
+    /// Класс, содержащий математические методы.
+    /// </summary>
+    public static class Maths
+    {
+        /// <summary>Вычисляет сумму двух чисел.</summary>
+        public static double Add(double a, double b) => a + b;
+
+        /// <summary>Вычисляет разность двух чисел.</summary>
+        public static double Subtract(double a, double b) => a - b;
+
+        /// <summary>Вычисляет произведение двух чисел.</summary>
+        public static double Multiply(double a, double b) => a * b;
+
+        /// <summary>
+        /// Вычисляет частное двух чисел.
+        /// </summary>
+        /// <param name="a">Делимое.</param>
+        /// <param name="b">Делитель (не должен быть 0).</param>
+        /// <returns>Результат деления.</returns>
+        /// <exception cref="System.DivideByZeroException">Возникает при делении на 0.</exception>
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Деление на ноль невозможно.");
+            return a / b;
+        }
+
+        /// <summary>
+        /// Вычисляет площадь прямоугольника.
+        /// </summary>
+        /// <param name="width">Ширина.</param>
+        /// <param name="height">Высота.</param>
+        /// <returns>Площадь прямоугольника.</returns>
+        /// <exception cref="System.ArgumentException">Возникает при отрицательных или нулевых значениях.</exception>
+        public static double RectangleArea(double width, double height)
+        {
+            if (width <= 0 || height <= 0)
+                throw new ArgumentException("Ширина и высота должны быть больше 0.");
+            return width * height;
+        }
+
+        /// <summary>Коэффициент перевода в бинарную систему (1024).</summary>
+        public const int BINARY_FACTOR = 1024;
+
+    }
+}
---
+++ LabWork23/LabWorkLibrary/User.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>Класс пользователя с логином, паролем и ролью.</summary>
+public class User
+{
+    /// <summary>Логин пользователя.</summary>
+    public string Login { get; set; }
+
+    /// <summary>Пароль пользователя.</summary>
+    public string Password { get; set; }
+
+    /// <summary>Роль пользователя.</summary>
+    public Role UserRole { get; set; }
+
+    /// <summary>Создает нового пользователя.</summary>
+    /// <param name="login">Логин.</param>
+    /// <param name="password">Пароль.</param>
+    /// <param name="role">Роль.</param>
+    public User(string login, string password, Role role)
+    {
+        Login = login;
+        Password = password;
+        UserRole = role;
+    }
+}
