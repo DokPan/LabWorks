003d5b39cae9ae970e9e4cc87cf01202530bc068
Alina <alinaroshchina5@gmail.com>
2025-03-31 09:58:25 +0300

    Order

--- LabWork24/Order.cs
+++ LabWork24/Order.cs
@@ -3,37 +3,41 @@ public class Order
 {
     private double total;
     private string address;
-
-    public int Id { get; set; }
+    const double TaxRate = 0.2;
+    const double DiscountThreshold = 10000;
+    const double Discount = 0.1;
 
     public double Total
     {
         get => total;
         set
         {
-            if (value < 0) throw new ArgumentException("Стоимость заказа не может быть отрицательной.");
+            if (value < 0)
+                throw new ArgumentException("Стоимость заказа не может быть отрицательной");
             total = value;
         }
     }
-
-    public bool IsExpress { get; set; }
 
     public string Address
     {
         get => address;
         set
         {
-            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("Адрес не может быть пустым.");
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Адрес не может быть пустым");
             address = value;
         }
     }
 
+    public int Id { get; set; }
+
+    public bool IsExpress { get; set; }
+
     public double CalculateTotalPrice()
     {
-        const double TaxRate = 0.2;
-        double discount = Total > 10000 ? Total * 0.1 : 0;
+        double discount = Total > DiscountThreshold ? Total * Discount : 0;
         double deliveryCost = DeliveryCostCalculator.CalculateDeliveryCost(this);
 
-        return Total - discount + (Total * TaxRate) + deliveryCost;
+        return Total - discount + Total * TaxRate + deliveryCost;
     }
 }
