c278084c8e965c42a58b534a87a4b654d3c180ed
Alina <alinaroshchina5@gmail.com>
2025-03-31 10:01:01 +0300

    Program

--- LabWork24/Program.cs
+++ LabWork24/Program.cs
@@ -2,7 +2,6 @@ OrderService orderService = new();
 
 var order = new Order { Id = 1, Total = 1200, IsExpress = true, Address = "Папанина, 24" };
 orderService.AddOrder(order);
-
 orderService.PrintOrderDetails(order.Id);
 
 Console.WriteLine("Полная стоимость: " + order.CalculateTotalPrice());
